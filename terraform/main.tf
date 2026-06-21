341f1938eea59d08445c4a14272619360266c66d
nourhan36 <nourhanibrahim2004@gmail.com>
2026-06-21 17:39:40 +0300

    Add ASG module

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,3 +49,11 @@ module "alb" {
   project_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
 }
+
+module "asg" {
+  source             = "./modules/asg"
+  project_name       = var.project_name
+  private_subnet_ids = module.vpc.private_app_subnet_ids
+  launch_template_id = module.launch_template.launch_template_id
+  target_group_arn   = module.alb.target_group_arn
+}
